bcdfee29754e2d1fbe710153ff12645a87218b40
Ivan <ivanecankov@gmail.com>
2024-04-28 20:16:17 +0200

    Added error manager and Exceptions

---
+++ backend/src/main/java/rest/ValidationErrorRestDto.java
@@ -0,0 +1,12 @@
+package rest;
+
+import java.util.List;
+
+/**
+ * A DTO class representing validation errors in a RESTful API response.
+ * This class encapsulates a message describing the overall validation
+ * error and a list of individual error messages.
+ */
+public record ValidationErrorRestDto(String message, List<String> errors) {
+}
+
